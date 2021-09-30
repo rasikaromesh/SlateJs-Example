05a6dfa621673caa59fa2bd79b295a6aa6d943b0
rasikaromesh <rasikaromesh000@gmail.com>
2021-09-30 14:04:39 +0800

    with codeElement and paragraphElement

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EditorComponent from './Components/Editor/EditorComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <EditorComponent />
+    </div>
+  );
+}
+
+export default App;
